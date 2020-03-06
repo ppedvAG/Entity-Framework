a10e3e116e0a719879f8140c4286232791d673f4
André <andrer@ppedv.de>
2020-03-06 11:19:24 +0100

    noSQL

---
+++ LiteDB_NoSQL/LiteDB_NoSQL/Program.cs
@@ -0,0 +1,48 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB_NoSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Auto() { PS = 555, Name = "Brum Burmm" };
+
+            using (var db = new LiteDatabase(@"C:\Temp\MyData222.db"))
+            {
+                 var table = db.GetCollection<Auto>("Autos");
+                // table.Insert(a);
+
+                foreach (var item in table.Query().ToList())
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                //var results = table.Query();.Where(x => x.Name.StartsWith("J")).OrderBy(x => x.Name).Select(x => new { x.Name, NameUpper = x.Name.ToUpper() }).Limit(10).ToList();
+
+
+            }
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+    class Auto
+    {
+
+        public int PS { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+
+}
