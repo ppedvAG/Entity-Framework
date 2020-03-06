fb8ae843af4599705ef3473ada503b5bbcef70b2
André <andrer@ppedv.de>
2020-03-06 12:01:46 +0100

    Consolen UI Läuft

---
+++ ppedv.Finex/ppedv.Finex.UI.CoreConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.Finex.Logic;
+using ppedv.Finex.Model;
+using System;
+
+namespace ppedv.Finex.UI.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** FINEX ***");
+
+            var core = new Core();
+            foreach (var med in core.Repository.GetAll<Medikament>())
+            {
+                Console.WriteLine($"{med.Name} {med.Nummer} {med.Preis:c}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
