152d55cc103ee760590b38b5274bf069452bd3f7
André <andrer@ppedv.de>
2020-03-06 16:41:09 +0100

    Jetzt mit Webseit und REST API

---
+++ ppedv.Finex/ppedv.Finex.UI.Web/Controllers/MediApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Finex.Logic;
+using ppedv.Finex.Model;
+
+namespace ppedv.Finex.UI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediApiController : ControllerBase
+    {
+        Core core = new Core();
+        // GET: api/MediApi
+        [HttpGet]
+        public IEnumerable<Medikament> Get()
+        {
+            return core.Repository.Query<Medikament>().ToList();
+        }
+
+        // GET: api/MediApi/5
+        [HttpGet("{id}", Name = "Get")]
+        public Medikament Get(int id)
+        {
+            return core.Repository.GetById<Medikament>(id);
+        }
+
+        // POST: api/MediApi
+        [HttpPost]
+        public void Post([FromBody] Medikament value)
+        {
+             core.Repository.Add<Medikament>(value);
+            core.Repository.SaveChanges();
+        }
+
+        // PUT: api/MediApi/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Medikament value)
+        {
+            core.Repository.Update<Medikament>(value);
+            core.Repository.SaveChanges();
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ppedv.Finex/ppedv.Finex.UI.Web/Controllers/MediController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Finex.Logic;
+using ppedv.Finex.Model;
+
+namespace ppedv.Finex.UI.Web.Controllers
+{
+    public class MediController : Controller
+    {
+        Core core = new Core();
+
+        // GET: Medi
+        public ActionResult Index()
+        {
+            return View(core.Repository.Query<Medikament>().ToList());
+        }
+
+        // GET: Medi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(core.Repository.GetById<Medikament>(id));
+        }
+
+        // GET: Medi/Create
+        public ActionResult Create()
+        {
+            return View(new Medikament() { Name = "NEU", Packungsgröße = 17 });
+        }
+
+        // POST: Medi/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Medikament medi)
+        {
+            try
+            {
+                core.Repository.Add(medi);
+                core.Repository.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Medi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(core.Repository.GetById<Medikament>(id));
+        }
+
+        // POST: Medi/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Medikament medi)
+        {
+            try
+            {
+                core.Repository.Update(medi);
+                core.Repository.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Medi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(core.Repository.GetById<Medikament>(id));
+        }
+
+        // POST: Medi/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Medikament medi)
+        {
+            try
+            {
+                var loaded = core.Repository.GetById<Medikament>(id);
+                if (loaded != null)
+                {
+                    core.Repository.Delete(loaded);
+                    core.Repository.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
