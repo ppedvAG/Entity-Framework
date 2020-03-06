79575610a21efb97dda1b7204cd1e7fb41306010
André <andrer@ppedv.de>
2020-03-06 11:46:43 +0100

    Logic mit Repo fertig

---
+++ ppedv.Finex/ppedv.Finex.Logic/Core.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.Finex.Model.Contracts;
+using System;
+
+namespace ppedv.Finex.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+    }
+}
