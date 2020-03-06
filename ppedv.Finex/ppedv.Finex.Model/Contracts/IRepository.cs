589475b727539b6ac32cf8adef7f1d90874c0d4f
André <andrer@ppedv.de>
2020-03-06 12:15:07 +0100

    Queries cool

--- ppedv.Finex/ppedv.Finex.Model/Contracts/IRepository.cs
+++ ppedv.Finex/ppedv.Finex.Model/Contracts/IRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ppedv.Finex.Model.Contracts
 {
     public interface IRepository
     {
-        IEnumerable<T> GetAll<T>() where T : Entity;
+        IQueryable<T> Query<T>() where T : Entity;
         T GetById<T>(int id) where T : Entity;
         void Add<T>(T entity) where T : Entity;
         void Delete<T>(T entity) where T : Entity;
--- ppedv.Finex/ppedv.Finex.UI.CoreConsole/Program.cs
+++ ppedv.Finex/ppedv.Finex.UI.CoreConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ppedv.Finex.Logic;
 using ppedv.Finex.Model;
 using System;
+using System.Linq;
+using System.Text;
 
 namespace ppedv.Finex.UI.CoreConsole
 {
@@ -8,12 +10,17 @@ namespace ppedv.Finex.UI.CoreConsole
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("*** FINEX ***");
 
             var core = new Core();
-            foreach (var med in core.Repository.GetAll<Medikament>())
+            foreach (var med in core.Repository.Query<Medikament>()
+                                               .Where(x => x.Modified.Year > 100)
+                                               .OrderBy(x => x.Lager.Sum(l => l.Bestand))
+                                               .ThenBy(x => x.Name)
+                                               .ToList())
             {
-                Console.WriteLine($"{med.Name} {med.Nummer} {med.Preis:c}");
+                Console.WriteLine($"[{med.Lager.Sum(l => l.Bestand)}] {med.Name} {med.Nummer} {med.Preis:c}");
             }
 
             Console.WriteLine("Ende");
